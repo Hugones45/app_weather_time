2e282f2421b6d35ee9d6a862e4f6c772be27d7a5
Hugo <hugones45@gmail.com>
2024-07-01 09:40:29 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const cityForm = document.querySelector('[data-js="change-location"')
+
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const citTyemperatureContainer = document.querySelector('[data-js="city-temperature"]')
+
+const cityCard = document.querySelector('[data-js="city-card"]')
+
+let timeImg = document.querySelector('[data-js="time"]')
+
+const timeIconContainer = document.querySelector('[data-js="time-icon"]')
+
+cityForm.addEventListener('submit', async e => {
+    e.preventDefault()
+
+    const inputValue = e.target.city.value
+    const [{ Key, LocalizedName }] = await getCityData(inputValue)
+    const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
+    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg"/>`
+
+    if (cityCard.classList.contains('d-none')) {
+        cityCard.classList.remove('d-none')
+    }
+
+    if (IsDayTime) {
+        timeImg.src = './src/day.svg'
+    } else {
+        timeImg.src = './src/night.svg'
+    }
+
+    timeIconContainer.innerHTML = timeIcon
+    cityNameContainer.textContent = LocalizedName
+    cityWeatherContainer.textContent = WeatherText
+    citTyemperatureContainer.textContent = Temperature.Metric.Value
+
+    cityForm.reset()
+})
---
+++ js/weather.js
@@ -0,0 +1,29 @@
+const APIkey = "ub4rdc9RkF1u6wNVB0kfkBXGJXOeG7kJ"
+const baseUrl = "http://dataservice.accuweather.com/"
+
+const getCityUrl = cityName => `${baseUrl}locations/v1/cities/search?apikey=${APIkey}&q=${cityName}`
+
+const getWeatherUrl = cityKey => `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIkey}`
+
+const fetchData = async url => {
+    try {
+
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error("Unable to retrieve the data.")
+        }
+
+        return response.json()
+
+    } catch ({ name, message }) {
+        alert(`${name}? ${message}`)
+    }
+}
+
+const getCityData = cityName => fetchData((getCityUrl(cityName)))
+
+const getCityWeather = cityKey => fetchData(getWeatherUrl(cityKey))
+
+
+
